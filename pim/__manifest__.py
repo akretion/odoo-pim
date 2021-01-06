7d9fe1916359f6e2fa24c54b399589524bd7be02
Pierrick Brun <pierrick.brun@akretion.com>
2021-01-06 11:33:28 +0100

    [IMP] pim: black, isort, prettier

--- pim/__manifest__.py
+++ pim/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Product Information Management",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Akretion",
     "website": "http://www.shopinvader.com",
