0642bae75dcba4a4a6d9e0904bfb4b7ef5323b76
Benoit <benoit.guillot@akretion.com>
2023-06-02 14:56:44 +0200

    fix spare hook

--- attribute_set/hooks.py
+++ attribute_set/hooks.py
@@ -8,17 +8,12 @@ def post_load_hook():
         attrs = super(IrModelFields, self)._instanciate_attrs(field_data)
         if attrs and field_data.get("serialization_field_id"):
             serialization_record_id = field_data["serialization_field_id"]
-            try:
-                self.env.cr.execute(
-                    """SELECT name FROM ir_model_fields WHERE id = %s""",
-                    (serialization_record_id,),
-                )
-                name = self.env.cr.fetchall()
-                attrs["sparse"] = name[0][0]
-            except AttributeError:
-                # due to https://github.com/OCA/odoo-pim/issues/134
-                # because depends_context isn't filled yet
-                attrs["sparse"] = None
+            self.env.cr.execute(
+                """SELECT name FROM ir_model_fields WHERE id = %s""",
+                (serialization_record_id,),
+            )
+            name = self.env.cr.fetchall()
+            attrs["sparse"] = name[0][0]
         return attrs
 
     IrModelFields._instanciate_attrs = _instanciate_attrs
